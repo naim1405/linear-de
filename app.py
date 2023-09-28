27836556961e8bc343c03bf819da26ae93a5803c
Naeem1405 <naim.baijid@gmail.com>
2023-09-28 12:17:27 +0600

    error handling and style overhaul

--- app.py
+++ app.py
@@ -2,32 +2,32 @@ from flask import Flask , redirect, url_for, render_template, request
 from solver import *
 
 
-
-
 app = Flask(__name__)
 
-
-@app.route("/", methods=["POST", "GET"])
+@app.route("/")
 def home():
-    if(request.method == 'POST'):
-        # print("POST REQ")
+    return render_template("index.html")
 
+@app.route("/solve", methods=["POST", "GET"])
+def solver():
+    if(request.method == 'POST'):
         ddx = request.form['ddx']
         c_y = request.form['c_y']
         const = request.form['const']
-        # print(const)
         eqn = get_eqn(ddx, c_y, const)
-
         solv = solve(ddx, c_y, const)
-        # return redirect(url_for('soln',eqn = eqn, soln = solv))
-        return render_template("soln.html", eqn = eqn, soln = solv)
+        return render_template("solution.html", eqn = eqn, soln = solv)
     
-    return render_template("form.html")
-    
-@app.route("/soln")
-def soln(eqn, soln):
-    return render_template("soln.html", eqn = eqn, soln = soln)
+    return render_template("equation-solver.html")
+
+@app.route("/about")
+def about():
+    return render_template("about-our-team.html")
+
+@app.route("/manual")
+def manual():
+    return render_template("user-manual.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # app.run()
+    # app.run(debug=True)
+    app.run()
--- solver.py
+++ solver.py
@@ -2,62 +2,44 @@ import sympy as smp
 from sympy import *
 import latex2mathml.converter
 
-# Saving the equation solve in png file
-
-eqn_path = './static/equation.png'
-solve_path = './static/solve.png'
-
-def latex_to_png(latex_str, file_path):
-    fig = plt.figure()
-    fig.set_figheight(1)
-    plt.axis("off")
-    plt.text(0.5, 0.5, f"${latex_str}$", size = 25, ha="center", va="center")
-    buffer = io.BytesIO()
-    plt.savefig(buffer, format="png", dpi=300)
-    plt.close(fig)
-    with open(file_path, 'wb') as f:
-        f.write(buffer.getvalue())
-    return
-    
-
+# generates the equation from coefficient and returns in mathML format
 def get_eqn(ddx, c_y, const):
     x, y, c = smp.symbols('x y c')
     euler_dict = {'e': E}
-    ddx = sympify(ddx,locals=euler_dict)
-    c_y = sympify(c_y,locals=euler_dict)
-    const = sympify(const,locals=euler_dict)
-    eq = Eq(Derivative(y,x) * ddx + c_y * y, const)
-    eq = latex(eq)
-    return latex2mathml.converter.convert(eq)
+    try:
+        ddx = sympify(ddx,locals=euler_dict)
+        c_y = sympify(c_y,locals=euler_dict)
+        const = sympify(const,locals=euler_dict)
+        eq = Eq(Derivative(y,x) * ddx + c_y * y, const)
+        eq = latex(eq)
+        return latex2mathml.converter.convert(eq)
+    except SympifyError:
+        return "Incorrect quation format"
 
 
 # Function to solve the linear differential equation
 def solve(ddx, c_y, const):
     x, y, c = smp.symbols('x y c')
     euler_dict = {'e': E}
-    ddx = sympify(ddx,locals=euler_dict)
-    c_y = sympify(c_y,locals=euler_dict)
-    const = sympify(const,locals=euler_dict)
+    try:
+        ddx = sympify(ddx,locals=euler_dict)
+        c_y = sympify(c_y,locals=euler_dict)
+        const = sympify(const,locals=euler_dict)
 
-    c_y = c_y / ddx
-    const = const / ddx
-    ddx = 1
-    # # integrating factor
-    i_f = integrate(c_y, x)
-    i_f = smp.exp(i_f)
+        c_y = c_y / ddx
+        const = const / ddx
+        ddx = 1
+        # # integrating factor
+        i_f = integrate(c_y, x)
+        i_f = smp.exp(i_f)
 
-    # # Prints the solved equation
-    solve = (integrate(i_f * const))
-    solve = solve + c
-    solve = solve / i_f
-    solve = Eq(y, solve)
-    ans = latex(solve)
-    return latex2mathml.converter.convert(ans)
-
-
-
-
-# solve("1 + smp.sin(x)**2", "smp.sin(2*x)", "smp.cos(x)")
-# solve("1 + sin(x)**2", "sin(2*x)", "cos(x)")
-
-# solve("1 + sin(x)^2", "sin(2*x)", "cos(x)")
+        # # Prints the solved equation
+        solve = (integrate(i_f * const))
+        solve = solve + c
+        solve = solve / i_f
+        solve = Eq(y, solve)
+        ans = latex(solve)
+        return latex2mathml.converter.convert(ans)
+    except ValueError:
+        str = "Incorrect equation"
+        return str
