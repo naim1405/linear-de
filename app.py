1b932a1a8081304fa0c86f4907f84cc32f88c1ab
Naeem1405 <naim.baijid@gmail.com>
2023-09-29 00:04:27 +0600

    add solution for initial value

--- app.py
+++ app.py
@@ -8,17 +8,34 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/solve", methods=["POST", "GET"])
-def solver():
-    if(request.method == 'POST'):
-        ddx = request.form['ddx']
-        c_y = request.form['c_y']
-        const = request.form['const']
-        eqn = get_eqn(ddx, c_y, const)
-        solv = solve(ddx, c_y, const)
-        return render_template("solution.html", eqn = eqn, soln = solv)
-    
-    return render_template("equation-solver.html")
+
+
+@app.route("/solve/<initial_value>", methods=["POST", "GET"])
+def solver(initial_value):
+    if(initial_value == 'False'):
+        if(request.method == 'POST'):
+            ddx = request.form['ddx']
+            c_y = request.form['c_y']
+            const = request.form['const']
+            eqn = get_eqn(ddx, c_y, const)
+            solv = solver_func(ddx, c_y, const)
+            return render_template("solution.html", eqn = eqn, soln = solv)
+
+        return render_template("equation-solver.html")
+    else:
+        if(request.method == 'POST'):
+            ddx = request.form['ddx']
+            c_y = request.form['c_y']
+            const = request.form['const']
+            x_val = request.form['x_val']
+            y_val = request.form['y_val']
+            # print(x_val , "  ", y_val)
+            eqn = get_eqn(ddx, c_y, const)
+            solv = initial_val_solver_func(ddx, c_y, const, x_val, y_val)
+            return render_template("initial-value-solution.html", eqn = eqn, soln = solv)
+
+        return render_template("initial-value-equation-solver.html")
+ 
 
 @app.route("/about")
 def about():
@@ -29,5 +46,4 @@ def manual():
     return render_template("user-manual.html")
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run()
+    app.run(debug=True)
--- solver.py
+++ solver.py
@@ -17,8 +17,8 @@ def get_eqn(ddx, c_y, const):
         return "Incorrect quation format"
 
 
-# Function to solve the linear differential equation
-def solve(ddx, c_y, const):
+# Function to solve the linear differential equation with no initial value
+def solver_func(ddx, c_y, const):
     x, y, c = smp.symbols('x y c')
     euler_dict = {'e': E}
     try:
@@ -33,12 +33,43 @@ def solve(ddx, c_y, const):
         i_f = integrate(c_y, x)
         i_f = smp.exp(i_f)
 
-        # # Prints the solved equation
-        solve = (integrate(i_f * const))
-        solve = solve + c
-        solve = solve / i_f
-        solve = Eq(y, solve)
-        ans = latex(solve)
+        # Returns the solved equation
+        soln = (integrate(i_f * const))
+        soln = soln + c
+        soln = soln / i_f
+        soln = Eq(y, soln)
+        ans = latex(soln)
+        return latex2mathml.converter.convert(ans)
+    except ValueError:
+        str = "Incorrect equation"
+        return str
+
+# Function to solve the linear differential equation with initial value
+def initial_val_solver_func(ddx, c_y, const, x_val, y_val):
+    x, y, c = smp.symbols('x y c')
+    euler_dict = {'e': E}
+    try:
+        ddx = sympify(ddx,locals=euler_dict)
+        c_y = sympify(c_y,locals=euler_dict)
+        const = sympify(const,locals=euler_dict)
+
+        c_y = c_y / ddx
+        const = const / ddx
+        ddx = 1
+        # # integrating factor
+        i_f = integrate(c_y, x)
+        i_f = smp.exp(i_f)
+
+        # # Returns the solved equation
+        soln = (integrate(i_f * const))
+        soln = soln + c
+        soln = soln / i_f
+        soln = Eq(y, soln)
+        soln_2 = soln.subs(x, x_val)
+        soln_2 = soln_2.subs(y, y_val)
+        c_val = solve(soln_2, dict=True)[0][c]
+        soln = soln.subs(c, c_val)
+        ans = latex(soln)
         return latex2mathml.converter.convert(ans)
     except ValueError:
         str = "Incorrect equation"
