90a3b12ba9d9c1376fa70115643fdb04c15a101d
Naeem1405 <naim.baijid@gmail.com>
2023-10-02 16:27:44 +0600

    works with all variable instead of just x and y

--- solver.py
+++ solver.py
@@ -4,7 +4,14 @@ import latex2mathml.converter
 
 # generates the equation from coefficient and returns in mathML format
 def get_eqn(eqn_str):
-    x, y, c = smp.symbols('x y c')
+    var_x = ""
+    var_y = ""
+    for i in range(len(eqn_str)):
+        if(eqn_str[i] == 'd' and eqn_str[i+2] == '/' and eqn_str[i+3] == 'd'):
+            var_x = eqn_str[i+4]
+            var_y = eqn_str[i+1]
+
+    x, y, c = smp.symbols(f'{var_x} {var_y} c')
     euler_dict = {'e': E}
     eq_lhs_str, eq_rhs_str = eqn_str.split('=')
     try:
@@ -19,9 +26,17 @@ def get_eqn(eqn_str):
 
 # Function to solve the linear differential equation with no initial value
 def solver_func(eqn_str):
-    x, y, c = smp.symbols('x y c')
+    var_x = ""
+    var_y = ""
+    for i in range(len(eqn_str)):
+        if(eqn_str[i] == 'd' and eqn_str[i+2] == '/' and eqn_str[i+3] == 'd'):
+            var_x = eqn_str[i+4]
+            var_y = eqn_str[i+1]
+            break
+
+    x, y, c = smp.symbols(f'{var_x} {var_y} c')
     euler_dict = {'e': E}
-    eqn_str = eqn_str.replace("dy/dx", "Derivative(y, x)")
+    eqn_str = eqn_str.replace(f"d{var_y}/d{var_x}", f"Derivative({var_y}, {var_x})")
     eq_lhs_str, eq_rhs_str = eqn_str.split('=')
     
     try:
@@ -29,16 +44,17 @@ def solver_func(eqn_str):
         eqn_rhs = sympify(eq_rhs_str, locals=euler_dict)
         eq = Eq(eqn_lhs, eqn_rhs)
         poly_eqn = smp.Poly(eq, [smp.Derivative(y, x), y])
+
         # Extract the coefficients
         coeffs = poly_eqn.coeffs()
         ddx = coeffs[0]
         c_y = coeffs[1]
         const = coeffs[2] * -1
- 
+
         c_y = c_y / ddx
         const = const / ddx
         ddx = 1
-        # # integrating factor
+        # integrating factor
         i_f = integrate(c_y, x)
         i_f = smp.exp(i_f)
 
@@ -49,19 +65,27 @@ def solver_func(eqn_str):
         soln = Eq(y, soln)
         ans = latex(soln)
         return latex2mathml.converter.convert(ans)
-        return soln
+        
     except ValueError:
-        str = "Incorrect equation Value error"
+        str = "Incorrect equation"
         return str
     except IndexError:
-        str = "Incorrect equation index error"
+        str = "Incorrect equation"
         return str
 
 # Function to solve the linear differential equation with initial value
 def initial_val_solver_func(eqn_str, x_val, y_val):
-    x, y, c = smp.symbols('x y c')
+    var_x = ""
+    var_y = ""
+    for i in range(len(eqn_str)):
+        if(eqn_str[i] == 'd' and eqn_str[i+2] == '/' and eqn_str[i+3] == 'd'):
+            var_x = eqn_str[i+4]
+            var_y = eqn_str[i+1]
+            break
+
+    x, y, c = smp.symbols(f'{var_x} {var_y} c')
     euler_dict = {'e': E}
-    eqn_str = eqn_str.replace("dy/dx", "Derivative(y, x)")
+    eqn_str = eqn_str.replace(f"d{var_y}/d{var_x}", f"Derivative({var_y}, {var_x})")
     eq_lhs_str, eq_rhs_str = eqn_str.split('=')
     
     try:
